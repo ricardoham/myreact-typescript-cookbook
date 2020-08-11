d42b96d727a0c867e4ed7a2c7bc805d736042601
Ricardo Manoel <rdomanoel@gmail.com>
2020-08-11 18:41:20 -0300

    dev: removing yarn and add tags genereator

---
+++ src/components/TagsGenerator/indext.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+interface Props {
+  tags?: string[];
+  onAddTag?: (tags: any) => void;
+  onRemoveTag?: (tags: any) => void;
+}
+
+const Tags = ({ tags, onAddTag, onRemoveTag }: Props) => {
+  const [tagText, setTagText] = useState('');
+  const [tag, setTag] = useState<string[]>(tags || []);
+
+  const handleAddTag = (event: any) => {
+    if (tagText !== '' && event === 'Enter') {
+      onAddTag ? onAddTag([...tags, tagText]) : setTag([...tag, tagText]);
+      setTagText('');
+      return;
+    }
+  };
+
+  const removeTag = (index: number) => {
+    onRemoveTag
+      ? onRemoveTag(tags.filter((_: any, i: number) => index !== i))
+      : setTag(tag.filter((_: any, i: number) => index !== i));
+  };
+
+  return (
+    <StyledTag>
+      <StyledTag.Header>Tags</StyledTag.Header>
+      <ul>
+        {tags &&
+          tags.map((t: string, index: number) => (
+            <StyledTag.Item key={index}>
+              <StyledTag.ItemText>{t}</StyledTag.ItemText>
+              <RemoveIcon onClick={() => removeTag(index)} />
+            </StyledTag.Item>
+          ))}
+      </ul>
+      <StyledTag.Input
+        value={tagText}
+        onKeyUp={(e: any) => handleAddTag(e.key)}
+        onChange={(e: any) => setTagText(e.target.value)}
+        placeholder="Type to insert a Tag"
+      />
+    </StyledTag>
+  );
+};
+
+export default Tags;
