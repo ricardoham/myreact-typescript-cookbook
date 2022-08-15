483f858f49f9e76ac230e2c25e2985f9d0a689c9
Ricardo Manoel <rdomanoel@gmail.com>
2022-08-15 23:59:41 +0100

    dev: code block with syntax highlight

--- apps/myreact-typescript-cookbook/src/pages/PortalsPage/index.tsx
+++ apps/myreact-typescript-cookbook/src/pages/PortalsPage/index.tsx
@@ -6,6 +6,7 @@ import ModalFooter from 'components/Modal/ModalFooter';
 import ModalHeader from 'components/Modal/ModalHeader';
 import Modal from '../../components/Modal';
 import * as S from './styles';
+import CodeBlock from 'components/CodeBlock';
 
 const PortalsPage = (): JSX.Element => {
   const [showModal, setShowModal] = useState(false);
@@ -13,6 +14,21 @@ const PortalsPage = (): JSX.Element => {
     '/images/snippets/dom-no-portals.png',
     '/images/snippets/dom-with-portals.png',
   ];
+  const codeString = `
+    import React from 'react';
+    import { createPortal } from 'react-dom';
+
+    const Portal = (): React.ReactPortal => {
+      return createPortal(
+        <div>
+          <h1>Portal Code Example</h1>
+          <p>This is a code example of React Portals</p>
+        </div>
+      ),
+      document.body // Where the portal will inject
+    }
+  `;
+
   return (
     <section>
       <article>
@@ -21,6 +37,7 @@ const PortalsPage = (): JSX.Element => {
           Portals provide first-class way to render children into a DOM node
           that exists outside the DOM hierarchy of the parent component.
         </p>
+        <CodeBlock codeString={codeString} />
         <p>Bellow a example with a Modal Component</p>
       </article>
       <Modal
